3dfabc893eeea757e83ae7a33947469c02c2aa4a
CloudSalander <ruben.as532@gmail.com>
2025-05-29 10:26:47 +0200

    Added finishing message

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 define('MIN_NUM',1);
 define('MAX_NUM', 100);
+define('CONGRATS_MSG', "Es juernes y el cuerpo lo sabe!");
 
 $randomNumber = rand(MIN_NUM,MAX_NUM);
 
@@ -9,5 +10,7 @@ $inputNumber = intval(readline("Tip a number,please"));
 while($inputNumber != $randomNumber) {
     $inputNumber = intval(readline("Tip a number,please"));
 }
+
+echo CONGRATS_MSG.PHP_EOL;
 
 ?>
