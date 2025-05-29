81ac1c3642dee22452e391414cd8f992ff69d774
CloudSalander <ruben.as532@gmail.com>
2025-05-29 10:24:21 +0200

    Main program loop and finishing condition implemented

--- index.php
+++ index.php
@@ -3,6 +3,11 @@ define('MIN_NUM',1);
 define('MAX_NUM', 100);
 
 $randomNumber = rand(MIN_NUM,MAX_NUM);
-var_dump($randomNumber);
+
+$inputNumber = intval(readline("Tip a number,please"));
+
+while($inputNumber != $randomNumber) {
+    $inputNumber = intval(readline("Tip a number,please"));
+}
 
 ?>
