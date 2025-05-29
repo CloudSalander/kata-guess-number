490169c5b41413c4af7b2ab76bd8bc8b0f2b75cc
Rubén <ruben.as532@gmail.com>
2025-05-29 13:59:15 +0200

    Unnecesary consts removed from index.ph

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php 
 include('class/GuessNumber.php');
 
-define('MIN_NUM',1);
-define('MAX_NUM', 100);
-define('INPUT_MSG',"Tip a number,please ");
-define('CONGRATS_MSG', "Es juernes y el cuerpo lo sabe!");
-
 $guessNumber = new guessNumber();
 $guessNumber->play();
 
